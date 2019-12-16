41d2d2fecb70c8b2dfd3f182732d886d07d0555e
dongwei <dongwei@chint.com>
2019-12-16 16:33:47 +0800

    feat 增加深拷贝方法

--- common.js
+++ common.js
@@ -1,9 +1,8 @@
-/*
- * desc: 根据传入的时间范围获取范围内所有年月
- * @param {String}[start] 起始时间('2019-01')
- * @param {String}[end] 结束时间('2019-10')
- * @return {Array}[result] 时间范围数组
- * 
+/**
+ * @description: 根据传入的时间范围获取范围内所有年月
+ * @param {String} [start] 起始时间('2019-01')
+ * @param {String} [end] 结束时间('2019-10')
+ * @return: {Array} [result] 时间范围数组
  */
 function getYearAndMonth(start, end) {
     let result = [];
@@ -16,13 +15,13 @@ function getYearAndMonth(start, end) {
     let sMon;
     while (staYear <= endYear) {
         if (staYear === endYear) {
-            if(staMon === endMon){
+            if (staMon === endMon) {
                 return [start];
             }
             while (staMon <= endMon) {
                 sMon = staMon;
-                if(sMon< 10){
-                    sMon = '0'+sMon;
+                if (sMon < 10) {
+                    sMon = '0' + sMon;
                 }
                 result.push(`${staYear}-${sMon}`);
                 staMon++;
@@ -34,12 +33,42 @@ function getYearAndMonth(start, end) {
                 staYear++;
             }
             sMon = staMon;
-            if(sMon< 10){
-                sMon = '0'+sMon;
+            if (sMon < 10) {
+                sMon = '0' + sMon;
             }
             result.push(`${staYear}-${sMon}`);
             staMon++;
         }
     }
     return result;
+}
+
+/**
+ * @description: 判断是否为对象
+ * @param {any} [obj]
+ * @return: {Boolean}
+ */
+function isObject(obj) {
+    return typeof obj === 'object' && obj !== null;
+}
+
+/**
+ * @description: 深拷贝
+ * @param {any} [source]
+ * @param {WeakMap} [hash]
+ * @return: [target] 拷贝结果
+ */
+function deepClone(source, hash = new WeakMap()) {
+    if (!isObject(source)) return source;
+    if (hash.has(source)) return hash.get(source);
+    let target = Array.isArray(source) ? [] : {};
+    hash.set(source, target);
+    Reflect.ownKeys(source).forEach(key => {
+        if (isObject(source[key])) {
+            target[key] = deepClone(source[key], hash);
+        } else {
+            target[key] = source[key];
+        }
+    });
+    return target;
 }
