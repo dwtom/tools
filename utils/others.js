c7088eeef4027616ba0607614677c2ea72e29c25
dongwei <1119086531@qq.com>
2022-12-07 17:54:41 +0800

    feat: 添加公共方法

--- utils/others.js
+++ utils/others.js
@@ -3,7 +3,7 @@
  * @Author: Dong Wei
  * @Date: 2022-11-14 10:05:20
  * @LastEditors: Dong Wei
- * @LastEditTime: 2022-11-14 10:05:22
+ * @LastEditTime: 2022-12-07 17:53:55
  * @FilePath: \tools\utils\others.js
  */
 /**
@@ -26,4 +26,18 @@ export const loadFont = async ({ fontFamily, fontUrl }, callback) => {
       console.log(error);
     }
   }
+};
+
+/**
+ * @description: setTimeout的同步写法
+ * @param {function} callback 回调
+ * @param {number} time 延时时间
+ * @return {Promise}
+ */
+export const sleep = (callback, time = 500) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(callback());
+    }, time);
+  });
 };
