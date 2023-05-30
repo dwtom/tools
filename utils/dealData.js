28b8e756c14559086873e4aea9d80cf54ad6a97c
dwtom <1119086531@qq.com>
2023-05-30 21:48:37 +0800

    feat: 新增方法

--- utils/dealData.js
+++ utils/dealData.js
@@ -3,22 +3,20 @@
  * @Author: Dong Wei
  * @Date: 2022-03-17 13:58:57
  * @LastEditors: Dong Wei
- * @LastEditTime: 2023-03-16 09:41:21
+ * @LastEditTime: 2023-05-30 21:48:21
  * @FilePath: \tools\utils\dealData.js
  */
 // 判断是否为对象
 export const isObject = (obj) => {
-  return typeof obj === 'object' && obj !== null;
+  return typeof obj === "object" && obj !== null;
 };
 // 判断数据类型
 export const getDataType = (obj) => {
   const type = typeof obj;
-  if (type !== 'object') {
+  if (type !== "object") {
     return type;
   }
-  return Object.prototype.toString
-    .call(obj)
-    .replace(/^\[object (\S+)\]$/, '$1');
+  return Object.prototype.toString.call(obj).replace(/^\[object (\S+)\]$/, "$1");
 };
 
 /**
@@ -43,13 +41,13 @@ export const getStringSlice = (str, length) => {
  * @return {String}
  */
 export const getThousandsNum = (num, fixed = 0) => {
-  if (typeof num !== 'number') num = Number(num);
-  if (isNaN(num)) return '';
-  num = num.toFixed(fixed).split('.');
+  if (typeof num !== "number") num = Number(num);
+  if (isNaN(num)) return "";
+  num = num.toFixed(fixed).split(".");
   if (fixed !== 0) {
-    return [num[0].replace(/\d{1,3}(?=(\d{3})+$)/g, '$&,'), num[1]].join('.');
+    return [num[0].replace(/\d{1,3}(?=(\d{3})+$)/g, "$&,"), num[1]].join(".");
   } else {
-    return [num[0].replace(/\d{1,3}(?=(\d{3})+$)/g, '$&,')].join('');
+    return [num[0].replace(/\d{1,3}(?=(\d{3})+$)/g, "$&,")].join("");
   }
 };
 
@@ -59,7 +57,7 @@ export const getThousandsNum = (num, fixed = 0) => {
  * @param {String} children children字段名 默认为children
  * @return {Array}
  */
-export const getFlatTree = (arr, children = 'children') => {
+export const getFlatTree = (arr, children = "children") => {
   if (!Array.isArray(arr)) {
     return arr;
   }
@@ -83,24 +81,12 @@ export const getFlatTree = (arr, children = 'children') => {
  * @param {string} childrenKey 子节点id键名
  * @return {array}
  */
-export const setTreeNodes = (
-  arr,
-  parentNode,
-  parentIdKey,
-  currentIdKey,
-  childrenKey
-) => {
+export const setTreeNodes = (arr, parentNode, parentIdKey, currentIdKey, childrenKey) => {
   const tree = [];
   for (const item of arr) {
     let tempNode = null;
     if (item[parentIdKey] === parentNode[currentIdKey]) {
-      tempNode = setTreeNodes(
-        arr,
-        item,
-        parentIdKey,
-        currentIdKey,
-        childrenKey
-      );
+      tempNode = setTreeNodes(arr, item, parentIdKey, currentIdKey, childrenKey);
       if (tempNode.length) {
         item[childrenKey] = tempNode;
       }
@@ -122,17 +108,34 @@ export const setTreeNodes = (
  */
 export const loopTreeFromChild = ({ arr, childName }, fn) => {
   if (!Array.isArray(arr)) return [];
-  return arr.map(son => {
+  return arr.map((son) => {
     if (son[childName] && Array.isArray(son[childName])) {
-      son[childName] = loopTreeFromChild(son[childName], childName, fn)
+      son[childName] = loopTreeFromChild(son[childName], childName, fn);
     }
-    return fn(son)
-  })
-}
+    return fn(son);
+  });
+};
 
 // 字符串数组按照拼音首字母排序
 export const arraySortByPinYin = (arr) => {
   return arr.sort(function (param1, param2) {
-    return param1.localeCompare(param2, 'zh');
+    return param1.localeCompare(param2, "zh");
   });
+};
+
+/**
+ * @description: 用于echarts的对象合并
+ * @param {object} optionDefault 默认option
+ * @param {object} optionSupply 补充配置以及数据等
+ * @return {object}
+ */
+// import { cloneDeep, mergeWith } from "lodash";
+export const mergeObj = (optionDefault, optionSupply) => {
+  const customizer = (defaultVal, outerVal, key) => {
+    // 数组执行替换操作其余属性执行合并操作
+    if (Array.isArray(defaultVal) && key === "data") {
+      return outerVal;
+    }
+  };
+  return mergeWith(cloneDeep(optionDefault), optionSupply, customizer);
 };
